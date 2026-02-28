1920b46c132e28fbeec19df42cd494b68f0681e3
thomaz <thomazbastiaanssen@gmail.com>
2026-02-28 14:10:41 +0100

    Further cleanup post integrating stack() into weave().

--- R/weave-formula-utils.R
+++ R/weave-formula-utils.R
@@ -1,19 +1,3 @@
-#' Combine LinkMaps by stacking features of multiple types.
-#' @description
-#' Utility to resolve multiple paths
-#' @param x `MultiFactor`
-#' @param .by formula
-#' @examples
-#' x <- randomMultiFactor()
-#' .stack_by_formula(x, a + c ~ d + e)
-#'
-#' @noRd
-#'
-.stack_by_formula <- function(x, .by) {
-    all_terms <- .weave_parse_formula(.by)
-
-    .stack_by_character(x, all_terms[[1L]], all_terms[[2L]])
-}
 
 #' Standardize terms
 #' @importFrom rlang f_lhs f_rhs
@@ -30,52 +14,3 @@
     lapply(list(y_vars, x_vars), all.vars)
 }
 
-#' Combine LinkMaps by stacking features of multiple types.
-#' @description
-#' Utility to resolve multiple paths
-#' @param x `MultiFactor`
-#' @param y_vars,x_vars `Character vector` names of feature types in left and
-#'     right columns of output, respectively.
-#' @returns `LinkMap`
-#' @noRd
-#'
-.stack_by_character <- function(x, y_vars, x_vars) {
-    # Defensive
-    if( length(y_vars) == 0L || length(x_vars) == 0L ) {
-        stop("Variables must be specified.")
-    }
-    stopifnot(
-        "Same variable may not appear in both arguments." =
-            length(intersect(y_vars, x_vars)) == 0L
-    )
-    if( length(miss <- setdiff(c(y_vars, x_vars), colnames(x))) >= 1L ) {
-        stop(
-            "Variables '", paste(miss, collapse = "', '"), "' not found in 'x'."
-        )
-    }
-    # Check combinations
-    var_grid <- expand.grid(y_vars, x_vars)
-    y_dupes  <- apply(var_grid, 1L, function(v) all(v %in% y_vars))
-    x_dupes  <- apply(var_grid, 1L, function(v) all(v %in% x_vars))
-    var_list <- apply(var_grid, 1L, `[`, simplify = FALSE)
-
-    i <- vapply(var_list, rowsWithCol, d = x@map, names = FALSE, FUN.VALUE = 0L)
-
-    tot_vars <- unique(unlist(var_list[i > 0L], use.names = FALSE))
-    y_name   <- paste(intersect(y_vars, tot_vars), collapse = ".")
-    x_name   <- paste(intersect(x_vars, tot_vars), collapse = ".")
-
-    x_sub <- lapply(
-        S7::S7_data(x)[i], function(y) {
-            # Ensure order: bools + 1L to get 1 and 2, where 2 is the x-column.
-            y <- y[, 1L + colnames(y) %in% x_vars ]
-            colnames(y) <- c(y_name, x_name)
-            y
-        }
-    )
-   do.call(
-        rbind.data.frame,
-        args = c(x_sub, make.row.names = FALSE, stringsAsFactors = TRUE)
-    )
-
-}
--- R/weave.R
+++ R/weave.R
@@ -3,7 +3,8 @@
 #' @rdname weave-methods
 #' @description
 #' Generates a new `LinkMap` object by cross-referencing the elements of a
-#'     given `MultiFactor`
+#'     given `MultiFactor`. Elements can be merged by including several names,
+#'     separated by the plus (`+`) sign. See examples.
 #' @param x a `MultiFactor`
 #' @param .by either a `formula` or a `character vector` of length 2 with the
 #'     names of the desired combination of feature types.
@@ -27,20 +28,20 @@
 #' # Weave new b2c LinkMap
 #' weave(x, b ~ c)
 #' weave(x, b ~ a, out.format = "matrix")
+#'
 NULL
 
 #' @export
 #'
 S7::method(weave, MultiFactor) <- function(x, .by, out.format = c("LinkMap", "matrix")) {
     out.format <- match.arg(out.format, c("LinkMap", "matrix"))
-    x <- MultiFactor(x)
     terms <- .by_terms(.by)
 
     #Stacking case
     if( any(lengths(terms) > 1L) ) {
         x <- .stack_by_character(x, terms[[1L]], terms[[2L]])
         if(out.format == "matrix") {
-            res <- `as.matrix.MultiFactor::LinkMap`(res)
+            res <- `as.matrix.MultiFactor::LinkMap`(x)
             dimnames(res) <- levels(x)[terms]
             return(res)
         } else
@@ -71,6 +72,88 @@ S7::method(weave, MultiFactor) <- function(x, .by, out.format = c("LinkMap", "ma
     LinkMap(res)
 }
 
+#' @export
+#' @importFrom utils stack
+#'
+S7::method(stack, MultiFactor) <- function(x, .by, out.format = c("LinkMap", "matrix"), ...) {
+    out.format <- match.arg(out.format, c("LinkMap", "matrix"))
+    terms <- .by_terms(.by)
+
+    #Stacking case
+    if( all( lengths(terms) == 1L ) ) stop(
+        "At least one side of formula argument `'.by' ",
+        "must contain more than one variable."
+    )
+
+    x <- .stack_by_character(x, terms[[1L]], terms[[2L]])
+    if(out.format == "matrix") {
+        res <- `as.matrix.MultiFactor::LinkMap`(x)
+        dimnames(res) <- levels(x)[vapply(terms, paste, collapse = ".", "")]
+        return(res)
+    }
+    if( out.format == "LinkMap" ) return(x)
+
+}
+
+#' @noRd
+#'
+.stack_by_formula <- function(x, .by) {
+    all_terms <- .weave_parse_formula(.by)
+
+    .stack_by_character(x, all_terms[[1L]], all_terms[[2L]])
+}
+
+#' Combine LinkMaps by stacking features of multiple types.
+#' @description
+#' Utility to resolve multiple paths
+#' @param x `MultiFactor`
+#' @param y_vars,x_vars `Character vector` names of feature types in left and
+#'     right columns of output, respectively.
+#' @returns `LinkMap`
+#' @noRd
+#'
+.stack_by_character <- function(x, y_vars, x_vars) {
+    # Defensive
+    if( length(y_vars) == 0L || length(x_vars) == 0L ) {
+        stop("Variables must be specified.")
+    }
+    stopifnot(
+        "Same variable may not appear in both arguments." =
+            length(intersect(y_vars, x_vars)) == 0L
+    )
+    if( length(miss <- setdiff(c(y_vars, x_vars), colnames(x))) >= 1L ) {
+        stop(
+            "Variables '", paste(miss, collapse = "', '"), "' not found in 'x'."
+        )
+    }
+    # Check combinations
+    var_grid <- expand.grid(y_vars, x_vars)
+    y_dupes  <- apply(var_grid, 1L, function(v) all(v %in% y_vars))
+    x_dupes  <- apply(var_grid, 1L, function(v) all(v %in% x_vars))
+    var_list <- apply(var_grid, 1L, `[`, simplify = FALSE)
+
+    i <- vapply(var_list, rowsWithCol, d = x@map, names = FALSE, FUN.VALUE = 0L)
+
+    tot_vars <- unique(unlist(var_list[i > 0L], use.names = FALSE))
+    y_name   <- paste(intersect(y_vars, tot_vars), collapse = ".")
+    x_name   <- paste(intersect(x_vars, tot_vars), collapse = ".")
+
+    x_sub <- lapply(
+        S7::S7_data(x)[i], function(y) {
+            # Ensure order: bools + 1L to get 1 and 2, where 2 is the x-column.
+            y <- y[, 1L + colnames(y) %in% x_vars ]
+            colnames(y) <- c(y_name, x_name)
+            y
+        }
+    )
+    do.call(
+        rbind.data.frame,
+        args = c(x_sub, make.row.names = FALSE, stringsAsFactors = TRUE)
+    )
+}
+
+
+
 # Utilities ----
 
 #' Standardize terms
@@ -90,40 +173,6 @@ S7::method(weave, MultiFactor) <- function(x, .by, out.format = c("LinkMap", "ma
             "Use formula syntax for more control."
         )
     }
-}
-
-#' Combine LinkMaps by stacking features of multiple types.
-#' @description
-#' Utility to resolve multiple paths
-#' @param x `MultiFactor`
-#' @param stack_vars `Character vector` names of feature types to combine
-#' @param to `Character scalar` name of feature type in right column of output.
-#' @returns `LinkMap`
-#' @noRd
-#'
-.stack_features <- function(x, stack_vars, to) {
-    stopifnot(
-        "At least two stacking targets must be found as colnames in 'x'" =
-            sum(c(stack_vars) %in% colnames(x)) >= 2L
-    )
-    stopifnot(
-        "'to' arg must be found in colnames of 'x'" = to %in% colnames(x)
-        )
-
-    var_list <- lapply(stack_vars, `c`, to)
-    i <- vapply(var_list, rowsWithCol, d = x@map, names = FALSE, FUN.VALUE = 0L)
-    var_list <- var_list[i > 0L]
-    stack_name <- paste(stack_vars[i > 0L], collapse = "8")
-    x_sub <- lapply(
-        S7::S7_data(x)[i], function(y) {
-            colnames(y)[colnames(y) != to] <- stack_name
-            y
-        }
-    )
-    LinkMap(
-        do.call(rbind.data.frame, list(unname(x_sub), make.row.names = FALSE ))
-        )
-
 }
 
 
